d7a7f40467bbaff4400ff2b1cae92a77814948b4
Roman <chess.terr.dev@yandex.ru>
2022-07-03 15:24:58 +0300

    In the "MultiProcessing" class, I limited the number of simultaneously running processes

--- DataBase/FillTables/FillTablesFromConsole.php
+++ DataBase/FillTables/FillTablesFromConsole.php
@@ -7,12 +7,30 @@ set_time_limit(9000);
 
 require_once __DIR__.'/../../vendor/autoload.php';
 
+use BoxberryListPoints\Libs\MultiProcessing;
+use BoxberryListPoints\Libs\Logger;
 
 
-$callbackFunction = function () {
-    (new \BoxberryListPoints\Libs\Logger('MultiProcessing'))
-        ->addLogInfo("Script started... \n");
+
+$scriptPath = __DIR__ . '/Worker.php';
+$param = json_decode(file_get_contents(\BoxberryListPoints\Configuration\Constants::TMP_PATH.'ListPoints.json'), true);
+
+$startCallbackFunction = function ($message) {
+    echo "The script is running: $message  \n";
+    (new Logger('Worker'))
+        ->addLogInfo("The script is running: $message \n");
+};
+$finishCallbackFunction = function ($message) {
+    echo "The script is completed: $message  \n";
+    (new Logger('Worker'))
+        ->addLogInfo("The script is completed: $message \n");
 };
 
-$arr = json_decode(file_get_contents(\BoxberryListPoints\Configuration\Constants::TMP_PATH.'ListPoints.json'), true);
-new \BoxberryListPoints\Libs\MultiProcessing(__DIR__ . '/Worker.php', $arr, $callbackFunction);
+$multiProcessing = new MultiProcessing($scriptPath, $param);
+$multiProcessing
+    ->setSumOfProcesses(10)
+    ->setStartCallbackFunction($startCallbackFunction)
+    ->setFinishCallbackFunction($finishCallbackFunction);
+
+$multiProcessing->run();
+
--- src/Libs/MultiProcessing.php
+++ src/Libs/MultiProcessing.php
@@ -6,44 +6,119 @@ use Symfony\Component\Process\Process;
 
 class MultiProcessing
 {
-    private array $pullProcess;
+    private array $pullProcess = [];
+    private string $scriptPath;
+    private array $params;
+    private int $sumOfProcesses = 50;
+    private ?\Closure $finishCallbackFunction = null;
+    private ?\Closure $startCallbackFunction  = null;
 
-    public function __construct(string $scriptPath, array $params, ?callable $callbackFunction = null)
+
+    /**
+     * @param string $scriptPath: the path to the php script that will be run
+     * @param array $params: the list of parameters that will be applied when running the script, how many elements are in the array, how much the script will be run
+     */
+    public function __construct(string $scriptPath, array $params)
     {
-        foreach ($params as $param) {
+        $this->scriptPath = $scriptPath;
+        $this->params = $params;
+    }
 
-            $this->initProcess($scriptPath, $param['Code'], $callbackFunction);
-        }
+    /**
+     * Starts the creation of processes
+     * @return mixed
+     */
+    public function run()
+    {
+        echo "Starting MultiProcessing \n";
 
         while (true) {
+            $this->initProcesses();
+
             sleep(1);
+
+            if (empty($this->pullProcess))  {
+                echo "Finished MultiProcessing \n";
+                return;
+            }
 
             $this->checkFinish();
         }
     }
 
-    private function initProcess(string $path, $code, ?callable $callbackFunction): Process
+    /**
+     * Starts the specified number of processes
+     * @return void
+     */
+    private function initProcesses(): void
     {
+        foreach ($this->params as $key => $param) {
+            if (count($this->pullProcess) >= $this->sumOfProcesses) return;
+
+            $this->initProcess($this->scriptPath, $param['Code']);
+
+            unset($this->params[$key]);
+        }
+    }
+
+    /**
+     * starts the process
+     * @param string $path
+     * @param $code
+     * @return \Symfony\Component\Process\Process
+     */
+    private function initProcess(string $path, $code): Process
+    {
+        $startCallbackFunction = $this->startCallbackFunction;
+
         $process = new Process(['php', $path, "--code=$code"]);
-        $process->start($callbackFunction);
+        $process->start($startCallbackFunction("--code=$code"));
 
         $this->pullProcess[$process->getPid() . ':' . $path] = $process;
 
         return $process;
     }
 
-    private function checkFinish()
+    /**
+     * @return void
+     */
+    private function checkFinish(): void
     {
-        if (empty($this->pullProcess))  return;
-
         foreach ($this->pullProcess as $nameProcess => $process) {
             if ($process instanceof Process && !$process->isRunning()) {
 
-                (new \BoxberryListPoints\Libs\Logger('MultiProcessing'))
-                    ->addLogInfo("Script #FINISHED: " . $nameProcess . ' ' . $process->getOutput() . "\n");
+                $finishCallbackFunction = $this->finishCallbackFunction;
+                $finishCallbackFunction($nameProcess . " : " . $process->getOutput());
 
                 unset($this->pullProcess[$nameProcess]);
             }
         }
+    }
+
+    /**
+     * @param \Closure|null $finishCallbackFunction finished init Process Callback Function
+     */
+    public function setFinishCallbackFunction(?\Closure $finishCallbackFunction)
+    {
+        $this->finishCallbackFunction = $finishCallbackFunction;
+        return $this;
+    }
+
+    /**
+     * @param \Closure|null $startCallbackFunction start init Process Callback Function
+     */
+    public function setStartCallbackFunction(?\Closure $startCallbackFunction)
+    {
+        $this->startCallbackFunction = $startCallbackFunction;
+        return $this;
+    }
+
+    /**
+     * @param int $sumOfProcesses number of simultaneously running processes
+     */
+    public function setSumOfProcesses(int $sumOfProcesses)
+    {
+        $this->sumOfProcesses = $sumOfProcesses;
+        return $this;
     }
 }

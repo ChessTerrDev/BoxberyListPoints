6956e100dc8211e8c610e5bfd59608fe5aa9ab4a
Roman <chess.terr.dev@yandex.ru>
2022-07-08 16:33:37 +0300

    Updated the database structure

--- DataBase/InstallTables/TableList.sql
+++ DataBase/InstallTables/TableList.sql
@@ -3,25 +3,26 @@ SET check_function_bodies = false;
 DROP VIEW IF EXISTS "viewProperties", "viewCountries", "viewAreas", "viewCities", "viewMetro", "viewWorkShedule", "viewAddresses", "viewGPS", "viewPhotos", "viewListPointsShort", "viewListPointsFull";
 DROP TABLE IF EXISTS "ListPoints","Countries","Addresses","Areas","Cities","Metro","Properties","WorkShedule","GPS","Photos";
 
+
 CREATE TABLE "ListPoints"
 (
-    id               serial                NOT NULL,
-    "Code"           character varying(90) NOT NULL,
-    "TerminalCode"   character varying(90),
-    "Name"           character varying(150),
-    "Organization"   character varying(150),
-    "ZipCode"        integer,
-    "CountryCode"    integer               NOT NULL,
-    "Area_id"        integer               NOT NULL,
-    "City_id"        integer               NOT NULL,
-    "Address_id"     integer               NOT NULL,
-    "GPS_id"         integer               NOT NULL,
-    "Property_id"    integer               NOT NULL,
-    "Phone"          character varying(90),
-    "WorkShedule_id" integer               NOT NULL,
-    "UpdateDate"     date                  NOT NULL,
-    "Active"         bool DEFAULT true,
-    "Deleted"        bool DEFAULT false,
+    id             serial                NOT NULL,
+    "Code"         character varying(90) NOT NULL,
+    "TerminalCode" character varying(90),
+    "Name"         character varying(150),
+    "Organization" character varying(150),
+    "ZipCode"      integer,
+    "CountryCode"  integer               NOT NULL,
+    "Area"         integer               NOT NULL,
+    "City"         integer               NOT NULL,
+    "Address"      integer               NOT NULL,
+    "GPS"          integer               NOT NULL,
+    "Properties"   integer               NOT NULL,
+    "Phone"        character varying(90),
+    "WorkShedule"  integer               NOT NULL,
+    "UpdateDate"   date                  NOT NULL,
+    "Active"       bool DEFAULT true,
+    "Deleted"      bool DEFAULT false,
     CONSTRAINT "ListPoints_pkey" PRIMARY KEY (id)
 );
 
@@ -156,32 +157,32 @@ ALTER TABLE "ListPoints"
         FOREIGN KEY ("CountryCode") REFERENCES "Countries" ("CountryCode");
 
 ALTER TABLE "ListPoints"
-    ADD CONSTRAINT "ListPoints_Area_id_fkey"
-        FOREIGN KEY ("Area_id") REFERENCES "Areas" (id);
+    ADD CONSTRAINT "ListPoints_Area_fkey"
+        FOREIGN KEY ("Area") REFERENCES "Areas" (id);
 
 ALTER TABLE "ListPoints"
-    ADD CONSTRAINT "ListPoints_City_id_fkey"
-        FOREIGN KEY ("City_id") REFERENCES "Cities" (id);
+    ADD CONSTRAINT "ListPoints_City_fkey"
+        FOREIGN KEY ("City") REFERENCES "Cities" (id);
 
 ALTER TABLE "Metro"
     ADD CONSTRAINT "Metro_City_id_fkey"
         FOREIGN KEY ("City_id") REFERENCES "Cities" (id);
 
 ALTER TABLE "ListPoints"
-    ADD CONSTRAINT "ListPoints_Address_id_fkey"
-        FOREIGN KEY ("Address_id") REFERENCES "Addresses" (id);
+    ADD CONSTRAINT "ListPoints_Address_fkey"
+        FOREIGN KEY ("Address") REFERENCES "Addresses" (id);
 
 ALTER TABLE "ListPoints"
-    ADD CONSTRAINT "ListPoints_GPS_id_fkey"
-        FOREIGN KEY ("GPS_id") REFERENCES "GPS" (id);
+    ADD CONSTRAINT "ListPoints_GPS_fkey" FOREIGN KEY ("GPS") REFERENCES "GPS" (id)
+;
 
 ALTER TABLE "ListPoints"
-    ADD CONSTRAINT "ListPoints_Property_id_fkey"
-        FOREIGN KEY ("Property_id") REFERENCES "Properties" (id);
+    ADD CONSTRAINT "ListPoints_Properties_fkey"
+        FOREIGN KEY ("Properties") REFERENCES "Properties" (id);
 
 ALTER TABLE "ListPoints"
-    ADD CONSTRAINT "ListPoints_WorkShedule_id_fkey"
-        FOREIGN KEY ("WorkShedule_id") REFERENCES "WorkShedule" (id);
+    ADD CONSTRAINT "ListPoints_WorkShedule_fkey"
+        FOREIGN KEY ("WorkShedule") REFERENCES "WorkShedule" (id);
 
 ALTER TABLE "Photos"
     ADD CONSTRAINT "Photos_ListPoint_id_fkey"
@@ -202,166 +203,3 @@ ALTER TABLE "Metro"
 ALTER TABLE "Addresses"
     ADD CONSTRAINT "Addresses_Cities_id_fkey"
         FOREIGN KEY ("Cities_id") REFERENCES "Cities" (id);
-
-CREATE VIEW "viewProperties" AS
-SELECT list."Code",
-       list."Name",
-       list."ZipCode",
-       prop."ForeignOnlineStoresOnly",
-       prop."PrepaidOrdersOnly",
-       prop."Acquiring",
-       prop."DigitalSignature",
-       prop."TypeOfOffice",
-       prop."CourierDelivery",
-       prop."Reception",
-       prop."ReceptionLaP",
-       prop."DeliveryLaP",
-       prop."LoadLimit",
-       prop."VolumeLimit",
-       prop."EnablePartialDelivery",
-       prop."EnableFitting",
-       prop."fittingType",
-       prop."NalKD",
-       prop."TransType",
-       prop."InterRefunds",
-       prop."ExpressReception",
-       prop."Terminal",
-       prop."IssuanceBoxberry"
-FROM "ListPoints" list
-         LEFT OUTER JOIN "Properties" prop ON list."Property_id" = prop."id";
-
-
-
-CREATE VIEW "viewCountries" AS
-SELECT list."Code", list."ZipCode", cnr."CountryCode", cnr."CountryName"
-FROM "ListPoints" list
-         LEFT OUTER JOIN "Countries" cnr ON list."CountryCode" = cnr."CountryCode";
-
-CREATE VIEW "viewAreas" AS
-SELECT list."Code", list."ZipCode", area."Name", cnr."CountryCode", cnr."CountryName"
-FROM "ListPoints" list
-         LEFT OUTER JOIN "Areas" area ON list."Area_id" = area.id
-         LEFT OUTER JOIN "Countries" cnr ON cnr."CountryCode" = area."CountryCode";
-
-CREATE VIEW "viewCities" AS
-SELECT list."Code",
-       list."ZipCode",
-       city."Name",
-       city."Settlement",
-       area."Name" as "AreaName",
-       cnr."CountryCode",
-       cnr."CountryName"
-FROM "ListPoints" list
-         LEFT OUTER JOIN "Cities" city ON list."City_id" = city.id
-         LEFT OUTER JOIN "Areas" area ON city."Area_id" = area.id
-         LEFT OUTER JOIN "Countries" cnr ON cnr."CountryCode" = area."CountryCode";
-
-CREATE VIEW "viewWorkShedule" AS
-SELECT wsh.*, list."Code", list."ZipCode"
-FROM "ListPoints" list
-         LEFT OUTER JOIN "WorkShedule" wsh ON list."WorkShedule_id" = wsh.id;
-
-CREATE VIEW "viewAddresses" AS
-SELECT addr.*, list."Code", list."ZipCode"
-FROM "ListPoints" list
-         LEFT OUTER JOIN "Addresses" addr ON list."Address_id" = addr.id;
-
-CREATE VIEW "viewGPS" AS
-SELECT gps.*, list."Code", list."ZipCode"
-FROM "ListPoints" list
-         LEFT OUTER JOIN "GPS" gps ON list."GPS_id" = gps.id;
-
-CREATE VIEW "viewListPointsShort" AS
-SELECT "Code", "TerminalCode", "Name", "Organization", "ZipCode", "UpdateDate"
-FROM "ListPoints";
-
-CREATE VIEW "viewListPointsFull" AS
-SELECT list."Code",
-       list."TerminalCode",
-       list."Name",
-       list."Organization",
-       list."ZipCode",
-       list."Area_id",
-       list."City_id",
-       list."Address_id",
-       list."GPS_id",
-       list."Property_id",
-       list."Phone",
-       list."WorkShedule_id",
-       list."UpdateDate",
-       list."Active",
-       list."Deleted",
-       property."ForeignOnlineStoresOnly",
-       property."PrepaidOrdersOnly",
-       property."Acquiring",
-       property."DigitalSignature",
-       property."TypeOfOffice",
-       property."CourierDelivery",
-       property."Reception",
-       property."ReceptionLaP",
-       property."DeliveryLaP",
-       property."LoadLimit",
-       property."VolumeLimit",
-       property."EnablePartialDelivery",
-       property."EnableFitting",
-       property."fittingType",
-       property."NalKD",
-       property."TransType",
-       property."InterRefunds",
-       property."ExpressReception",
-       property."Terminal",
-       property."IssuanceBoxberry",
-       addr."Street",
-       addr."House",
-       addr."Structure",
-       addr."Housing",
-       addr."Apartment",
-       addr."AddressFull",
-       addr."AddressReduce",
-       addr."TripDescription",
-       wsh."ShortWorkShedule",
-       wsh."WorkMoBegin",
-       wsh."WorkMoEnd",
-       wsh."WorkTuBegin",
-       wsh."WorkTuEnd",
-       wsh."WorkWeBegin",
-       wsh."WorkWeEnd",
-       wsh."WorkThBegin",
-       wsh."WorkThEnd",
-       wsh."WorkFrBegin",
-       wsh."WorkFrEnd",
-       wsh."WorkSaBegin",
-       wsh."WorkSaEnd",
-       wsh."WorkSuBegin",
-       wsh."WorkSuEnd",
-       wsh."LunchMoBegin",
-       wsh."LunchMoEnd",
-       wsh."LunchTuBegin",
-       wsh."LunchTuEnd",
-       wsh."LunchWeBegin",
-       wsh."LunchWeEnd",
-       wsh."LunchThBegin",
-       wsh."LunchThEnd",
-       wsh."LunchFrBegin",
-       wsh."LunchFrEnd",
-       wsh."LunchSaBegin",
-       wsh."LunchSaEnd",
-       wsh."LunchSuBegin",
-       wsh."LunchSuEnd",
-       wsh."ScheduleJSON",
-       ctr."CountryCode",
-       ctr."CountryName",
-       area."Name" AS "AreaName",
-       city."Name" AS "CityName",
-       city."Settlement",
-       gps."latitude",
-       gps."longitude"
-FROM "ListPoints" list
-         LEFT OUTER JOIN "Properties" property ON list."Property_id" = property.id
-         LEFT OUTER JOIN "Addresses" addr ON list."Address_id" = addr.id
-         LEFT OUTER JOIN "WorkShedule" wsh ON list."WorkShedule_id" = wsh.id
-         LEFT OUTER JOIN "Countries" ctr ON list."CountryCode" = ctr."CountryCode"
-         LEFT OUTER JOIN "Areas" area ON list."Area_id" = area.id
-         LEFT OUTER JOIN "Cities" city ON list."City_id" = city.id
-         LEFT OUTER JOIN "GPS" gps ON list."GPS_id" = gps.id
-;

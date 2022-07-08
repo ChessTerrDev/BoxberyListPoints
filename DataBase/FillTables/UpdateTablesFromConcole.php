07cc279c444c4fa0b2dd86506b97e28d12031ce3
Roman <chess.terr.dev@yandex.ru>
2022-07-08 17:15:32 +0300

    filling in and updating the database

--- DataBase/FillTables/UpdateTablesFromConcole.php
+++ DataBase/FillTables/UpdateTablesFromConcole.php
@@ -5,38 +5,68 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 set_time_limit(9000);
 
-require_once __DIR__.'/../../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 use BoxberryListPoints\Libs\MultiProcessing;
 use BoxberryListPoints\Libs\Logger;
-use BoxberryListPoints\Controllers\Update\ListPoints\ListPoints;
 use BoxberryListPoints\Controllers\Update\ListPoints\ListPointsShort;
+use BoxberryListPoints\Controllers\ListPointShort;
 
-//$listPoints = new ListPoints();
-//$listPoints->loadFullListPoints();
-//$listPoints->loadCityListPoints(643, '77400');
-//$listPoints->loadCountryListPoints(643);
+// Список записей из БД
+$listUpdateDate = [];
+$listPointsFromBD = ListPointShort::findAllPointsShort(['Code', 'UpdateDate']);
+foreach ($listPointsFromBD as $point)
+    $listUpdateDate[(string)$point['Code']] = $point['UpdateDate'];
+//unset($listPointsFromBD);
 
+// Список актуальных записей из БД Боксбери
 $listPoints = new ListPointsShort();
 $listPoints->loadFullListPoints();
 //$listPoints->loadCountryListPoints(643);
 //$listPoints->loadCityListPoints(643, '77400');
+$param = $listPoints->toArray();
 
+
+print_r(count($param));
+echo "\n";
+print_r(count($listPointsFromBD));
+echo "\n";
+print_r(count($listUpdateDate));
+echo "\n";
+
+
+// Оставляем только те записи которых нет в нашей базе или другая дата обновления
+$listPointsToUpdate = [];
+foreach ($param as $key => $point) {
+    $point['UpdateDate'] = preg_replace("/\s\d{2}:\d{2}:\d{2}/", '', $point['UpdateDate']);
+
+    if (isset($listUpdateDate[(string)$point['Code']]) && $listUpdateDate[(string)$point['Code']] !== $point['UpdateDate']) {
+        $point['oldData'] = $listUpdateDate[(string)$point['Code']];
+        $listPointsToUpdate[] = $point;
+    }
+}
+unset($listUpdateDate);
+unset($param);
+
+print_r($listPointsToUpdate);
+echo "\n";
 
 
 $scriptPath = __DIR__ . '/Worker.php';
-$params = $listPoints->toArray();
+
 
 $startCallbackFunction = function ($message) {
     echo "The script is running: $message  \n";
-    //(new Logger('Worker'))->addLogInfo("The script is running: $message \n");
+    (new Logger('Worker'))
+        ->addLogInfo("The script is running: $message \n");
 };
 $finishCallbackFunction = function ($message) {
     echo "The script is completed: $message  \n";
-    //(new Logger('Worker'))->addLogInfo("The script is completed: $message \n");
+    (new Logger('Worker'))
+        ->addLogInfo("The script is completed: $message \n");
 };
 
-$multiProcessing = new MultiProcessing($scriptPath, $params);
+$multiProcessing = new MultiProcessing($scriptPath, $listPointsToUpdate);
 $multiProcessing
     ->setSumOfProcesses(10)
     ->setStartCallbackFunction($startCallbackFunction)
--- DataBase/FillTables/Worker.php
+++ DataBase/FillTables/Worker.php
@@ -8,20 +8,31 @@ require_once '/var/www/phpstorm/BoxberyListPoints/vendor/autoload.php';
 use BoxberryListPoints\Controllers\Update\ListPoints\PointsDescription;
 use BoxberryListPoints\Controllers\ListPoint;
 
-$options = getopt("c::p::", ['code::', 'photo::']);
+/**
+ * Принимаем параметры
+ */
+$options = getopt("", ['Code::', 'UpdateDate::']);
+$code = $options["Code"] ?? null;
+$updateDate = $options["UpdateDate"] ?? null;
 
-if (empty($_GET['code']) && empty($options["code"]) && empty($options["c"]))
+if (empty($code))
     throw new \Exception('Пустой идентификатор ПВЗ!');
 
-$code = $_GET['code'] ?? $options["code"] ?? $options["c"];
 
+/**
+ * Получаем подробную информацию о ПВЗ
+ */
+$pointDescription = new PointsDescription((string)str_replace('"', '', $code));
+$description = $pointDescription
+    ->loadPointsDescription()
+    ->toArray();
 
+$description['UpdateDate'] = $updateDate ? preg_replace("/\s\d{2}:\d{2}:\d{2}/", '', str_replace('"', '', $updateDate)) : date('Y-m-d');
 
-$pointDescription = new PointsDescription((string)$code);
-$pointDescription->loadPointsDescription();
+var_dump($description);
+/**
+ * Добавляем запись в БД
+ */
+$result = ListPoint::addPoint($description);
 
-$pointController = new ListPoint();
-$result = $pointController->addPoint($pointDescription->toArray());
-
-(new \BoxberryListPoints\Libs\Logger('Worker'))
-    ->addLogInfo('Получен результат: ' . json_encode($result->getFields()));
+//(new*/ \BoxberryListPoints\Libs\Logger('Worker'))->addLogInfo('Получен результат: ' . json_encode($result->getFields()));

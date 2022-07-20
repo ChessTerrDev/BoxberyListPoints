8a63cdcf49f3e85074b433682fd4e5821de1769e
Roman <chess.terr.dev@yandex.ru>
2022-07-20 20:39:44 +0300

    add README.md

--- DataBase/InstallTables/InstallTables.php
+++ DataBase/InstallTables/InstallTables.php
@@ -1,9 +1,16 @@
 <?php
-header("Content-Type: text/html; charset=UTF-8");
-
-@ini_set('display_errors', 1);
-@error_reporting(E_ALL);
-
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 @set_time_limit(9000);
-//ini_set('memory_limit', '64M');
 @ini_set('max_execution_time', '9000');
+
+require_once '../vendor/autoload.php';
+
+$connection = new \BoxberryListPoints\DateBase\Connection();
+$dataBase = new \BoxberryListPoints\DateBase\DataBase($connection->connect());
+
+$sql = file_get_contents('./TableList.sql');
+$dataBase->execution($sql);
+
+echo "Выполнен запрос к базе данный на добавление новых таблиц! ";
